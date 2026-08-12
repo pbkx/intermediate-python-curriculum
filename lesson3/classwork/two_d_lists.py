d044727117818c87e2cf47bc457fcd5a53d1c328
pbkx <93405617+pbkx@users.noreply.github.com>
2026-08-12 21:27:49 +0000

    Finished lesson 3

---
+++ lesson3/classwork/two_d_lists.py
@@ -0,0 +1,18 @@
+# A 2D list can represent a grid
+board = [
+    [".", ".", "."],
+    [".", "X", "."],
+    [".", ".", "."]
+]
+
+# Print the board row by row
+for row in range(len(board)):
+    print(board[row])
+
+print("Accessed:")
+print(board[1])  # access one row in the 2D list
+
+board[0][2] = "O"
+print("After change:")
+for row in range(len(board)):
+    print(board[row])
